d1c6cb5296912778f11497dfd10e8aaa9011cd91
zullykul24 <zullykulphinh@gmail.com>
2020-06-12 01:01:43 +0700

    Close #5

--- game.js
+++ game.js
@@ -1,32 +1,35 @@
 var lesson = 0; 
 
-
-
-
 const numOfApples = [5, 10, 16, 20, 23, 7];
+const numOfLesson = numOfApples.length;
 const maxApples = 30;
-
-
-
 
 function check_result(x){
     var input_value = document.getElementById("input").value;
-    var input =  document.getElementById("input");
-    var alertBox = document.getElementById("alertBox");
+
+    
     var answer = document.getElementById("answer");
     var cap = document.getElementsByClassName("caption");
     var wrongCross = document.getElementById("wrongCross");
+    var messsage = document.getElementById("chucMung");
+    var next = document.getElementById("next");
     if(input_value == x){
         playAudio("right");
         setTimeout(function(){
-        alertBox.style.visibility = "visible";
-        
-        cap[0].style = "opacity : 0;";
-        answer.style = "opacity : 0;";
-        },500
-        )
-        //next();
-        lesson++;
+             
+            alertBox.style.visibility = "visible";
+            cap[0].style = "opacity : 0;";
+            answer.style = "opacity : 0;";
+        },500)
+        if(lesson == numOfLesson - 1){
+            messsage.innerHTML = "Bạn đã hoàn thành trò chơi! Chúc mừng";
+            next.innerHTML = "Chơi lại";
+            next.onclick = function(){
+                location.reload();
+            }
+            
+        }
+        else lesson++;
     }
     if(input_value != x){
         playAudio("wrong");
@@ -50,75 +53,51 @@ function next()
         var input = document.getElementById("input");
         var answer = document.getElementById("answer");
         var cap = document.getElementsByClassName("caption");
+        var alertBox = document.getElementById("alertBox");
+        var messsage = document.getElementById("chucMung");
+        var next = document.getElementById("next");
+
+
+
         setTimeout(function(){
             alertBox.style.visibility = "hidden";
             
+            
             cap[0].style = "opacity : 1;";
             answer.style = "opacity : 1;";
-            },500
-            )
+        },500)
         setTimeout(function(){
-        input.value = "";
-        
-        var apples = document.getElementsByClassName("plot");
-        for(var i = 0;i < numOfApples[lesson]; i++){
-        apples[i].style = "background-image: url('assets/apple.png');";
-        }
-        for(var i = numOfApples[lesson]; i < maxApples;i++){
-            apples[i].style = "background-image: none;";
-        }
-     },500) //reset
-    
+            input.value = "";
+            var apples = document.getElementsByClassName("plot");
+            for(var i = 0;i < numOfApples[lesson]; i++){
+            apples[i].style = "background-image: url('assets/apple.png');";
+            }
+            for(var i = numOfApples[lesson]; i < maxApples;i++){
+                apples[i].style = "background-image: none;";
+            }
+        },500) 
 }
 
-        
 
-
-
-
-    /// finish screen
-function finish_screen()
-{
-    var cap = document.getElementById("caption");
-
-    var answer = document.getElementsByClassName("answer");
-    for(i = 0;i < answer.length; i++){answer[i].style.display = "none";}
-
-
-    var question_1 = document.getElementById("question_1");
-    var question_2 = document.getElementById("question_2");
-    var question_3 = document.getElementById("question_3");
-
-    cap.innerHTML = "You've completed the game! Congratulations!" ;
-    cap.style = "font-size: 30px";
-
-    question_1.innerHTML = "Your score: "+rightBalls;
-    question_1.style = " font-size: 40px; text-align: center; line-height: 50px; width: 960px";
-
-    if(rightBalls == 5)question_1.innerHTML +="<br>Perfect!";
-    else question_1.innerHTML +="<br>Try better next time!";
-
-    question_2.style = " font-size: 40px; text-align: center; width: 960px;"; 
-    question_2.innerHTML = "<input type='submit' value='Restart' onclick= 'startButton()';>";// Click button to restart
-    question_3.style = " font-size: 40px; text-align: center; width: 960px;";
-    question_3.innerHTML = "<input type='submit' value='Back to menu' onclick = 'backToMenu()'>";//Click to go back to menu
-
-    var next_link = document.getElementById("next_link");
-    next_link.innerHTML = ""; // Next button disappears 
-    next_link.style.display = "none";
-
-}   
 function restart()
 {
+    lesson = 0; 
     playAudio("backgroundMusic");
     var apples = document.getElementsByClassName("plot");
     var contentncap = document.getElementById("contentncap");
+    var answer = document.getElementById("answer");
+    var cap = document.getElementsByClassName("caption");
+    var alertBox = document.getElementById("alertBox");
+
     for(let i = 0;i < numOfApples[lesson]; i++){
         apples[i].style = "background-image: url('assets/apple.png');";
-    }   //reset the variables;
-    lesson = 0; 
+    }  
+    
+    
    
-    var cap = document.getElementById("caption");
+    alertBox.style.visibility = "hidden";
+            cap[0].style = "opacity : 1;";
+            answer.style = "opacity : 1;";
     contentncap.style.visibility = "visible";
  
     
@@ -126,6 +105,7 @@ function restart()
 
     var img = document.getElementById("startButton");
     img.hidden = true;
+    console.log(lesson);
 
 }
 function startButton(){
@@ -139,6 +119,9 @@ function startButton(){
 }
 function playAudio(play) {
     document.getElementById(play).play();
+}
+function pauseAudio(pause){
+    document.getElementById(pause).pause();
 }
 
 
