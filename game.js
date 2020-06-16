c4cfef56db0475b39f25f61841afc0d9c0e164b4
zullykul24 <zullykulphinh@gmail.com>
2020-06-16 23:35:39 +0700

    close #6, close #8

--- game.js
+++ game.js
@@ -1,22 +1,19 @@
 var lesson = 0; 
-
-const numOfApples = [5, 10, 16, 20, 23, 7];
-const numOfLesson = numOfApples.length;
+const numOfLesson = 5;
+const numOfApples = [];
 const maxApples = 30;
-
+var backgroundMusic = true;
+var soundEffects = true;
 function check_result(x){
-    var input_value = document.getElementById("input").value;
-
-    
+    var input_value = document.getElementById("input").value; 
     var answer = document.getElementById("answer");
     var cap = document.getElementsByClassName("caption");
     var wrongCross = document.getElementById("wrongCross");
     var messsage = document.getElementById("chucMung");
     var next = document.getElementById("next");
     if(input_value == x){
-        playAudio("right");
-        setTimeout(function(){
-             
+        if(soundEffects == true){playAudio("right");}
+        setTimeout(function(){          
             alertBox.style.visibility = "visible";
             cap[0].style = "opacity : 0;";
             answer.style = "opacity : 0;";
@@ -27,12 +24,11 @@ function check_result(x){
             next.onclick = function(){
                 location.reload();
             }
-            
         }
         else lesson++;
     }
     if(input_value != x){
-        playAudio("wrong");
+        if(soundEffects == true){playAudio("wrong");}
         wrongCross.style = "background-size : 200px;"
         wrongCross.style.visibility = "visible";
         setTimeout(function(){
@@ -43,10 +39,7 @@ function check_result(x){
             wrongCross.style = "background-size : 0px;"
 
         },1500)
-
     }
-   
- 
 }
 function next()
 { 
@@ -56,13 +49,8 @@ function next()
         var alertBox = document.getElementById("alertBox");
         var messsage = document.getElementById("chucMung");
         var next = document.getElementById("next");
-
-
-
         setTimeout(function(){
-            alertBox.style.visibility = "hidden";
-            
-            
+            alertBox.style.visibility = "hidden";    
             cap[0].style = "opacity : 1;";
             answer.style = "opacity : 1;";
         },500)
@@ -77,10 +65,9 @@ function next()
             }
         },500) 
 }
-
-
 function restart()
-{
+{ 
+    for(var i = 0;i < numOfLesson;i++){numOfApples[i]= Math.floor((Math.random() * 30) + 1);}// generate array of random integers from 1 to 30
     lesson = 0; 
     playAudio("backgroundMusic");
     var apples = document.getElementsByClassName("plot");
@@ -89,24 +76,15 @@ function restart()
     var cap = document.getElementsByClassName("caption");
     var alertBox = document.getElementById("alertBox");
 
-    for(let i = 0;i < numOfApples[lesson]; i++){
+    for(var i = 0;i < numOfApples[lesson]; i++){
         apples[i].style = "background-image: url('assets/apple.png');";
     }  
-    
-    
-   
     alertBox.style.visibility = "hidden";
-            cap[0].style = "opacity : 1;";
-            answer.style = "opacity : 1;";
+    cap[0].style = "opacity : 1;";
+    answer.style = "opacity : 1;";
     contentncap.style.visibility = "visible";
- 
-    
-
-
     var img = document.getElementById("startButton");
     img.hidden = true;
-    console.log(lesson);
-
 }
 function startButton(){
     var img = document.getElementById("startButton");
@@ -117,11 +95,47 @@ function startButton(){
     contentncap.style.visibility = "hidden";
 
 }
+function enableBackgroundMusic(){
+    var music = document.getElementById("backgroundMusic");
+    var enableBackgroundMusic = document.getElementById("enableBackgroundMusic");
+    if(backgroundMusic == true){
+        music.pause();
+        backgroundMusic = false;
+        enableBackgroundMusic.style = "background-color: bisque;"
+        enableBackgroundMusic.innerHTML = "Nhạc nền: TẮT"
+    }
+    else{
+        music.play();
+        backgroundMusic = true;
+        enableBackgroundMusic.style = "background-color: aquamarine;"
+        enableBackgroundMusic.innerHTML = "Nhạc nền: BẬT"
+    }
+}
+function enableSoundEffects(){
+    var enableSoundEffects = document.getElementById("enableSoundEffects");
+    if(soundEffects == true){
+        soundEffects = false;
+        enableSoundEffects.style = "background-color: bisque;"
+        enableSoundEffects.innerHTML = "Hiệu ứng: TẮT"
+    }
+    else {
+        soundEffects = true;
+        enableSoundEffects.style = "background-color: aquamarine;"
+        enableSoundEffects.innerHTML = "Hiệu ứng: BẬT"
+    }
+}
+function loa(){
+    var coBaoNhieu = document.getElementById("coBaoNhieu");
+    if(soundEffects == true){
+        playAudio("coBaoNhieu");
+        coBaoNhieu.volume = 1;
+    }
+    else{
+        coBaoNhieu.volume = 0;
+    }
+}
 function playAudio(play) {
     document.getElementById(play).play();
-}
-function pauseAudio(pause){
-    document.getElementById(pause).pause();
 }
 
 
